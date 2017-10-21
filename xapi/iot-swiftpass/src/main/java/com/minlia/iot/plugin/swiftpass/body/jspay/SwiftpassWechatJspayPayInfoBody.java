f6026b7817fad0ee1729b3968487f15b9c15e934
garen <191285052@qq.com>
2017-10-21 10:16:17 +0800

    Update at 20171021101617 by dev/garen

--- xapi/iot-swiftpass/src/main/java/com/minlia/iot/plugin/swiftpass/body/jspay/SwiftpassWechatJspayPayInfoBody.java
+++ xapi/iot-swiftpass/src/main/java/com/minlia/iot/plugin/swiftpass/body/jspay/SwiftpassWechatJspayPayInfoBody.java
@@ -9,7 +9,6 @@ import javax.xml.bind.annotation.XmlElement;
 /**
  * Created by will on 9/10/17.
  */
-@Data
 public class SwiftpassWechatJspayPayInfoBody {
 
   private String appId;
@@ -23,10 +22,74 @@ public class SwiftpassWechatJspayPayInfoBody {
   @JsonProperty(value = "package")
   private String packages;
 
-  private String callbackUrl;
+  @JsonProperty(value = "callbackUrl")
+  private String callback_url;
 
   private String nonceStr;
 
   private String paySign;
 
+  public String getAppId() {
+    return appId;
+  }
+
+  public void setAppId(String appId) {
+    this.appId = appId;
+  }
+
+  public String getTimeStamp() {
+    return timeStamp;
+  }
+
+  public void setTimeStamp(String timeStamp) {
+    this.timeStamp = timeStamp;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getSignType() {
+    return signType;
+  }
+
+  public void setSignType(String signType) {
+    this.signType = signType;
+  }
+
+  public String getPackage() {
+    return packages;
+  }
+
+  public void setPackage(String packages) {
+    this.packages = packages;
+  }
+
+  public String getCallback_url() {
+    return callback_url;
+  }
+
+  public void setCallback_url(String callback_url) {
+    this.callback_url = callback_url;
+  }
+
+  public String getNonceStr() {
+    return nonceStr;
+  }
+
+  public void setNonceStr(String nonceStr) {
+    this.nonceStr = nonceStr;
+  }
+
+  public String getPaySign() {
+    return paySign;
+  }
+
+  public void setPaySign(String paySign) {
+    this.paySign = paySign;
+  }
 }
