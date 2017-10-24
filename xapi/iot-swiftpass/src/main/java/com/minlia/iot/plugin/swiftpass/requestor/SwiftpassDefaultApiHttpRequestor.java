cd2ee6b56571417e69a9b68b072957e1860e542e
garen <191285052@qq.com>
2017-10-24 09:46:45 +0800

    Update at 20171024094645 by dev/garen

--- xapi/iot-swiftpass/src/main/java/com/minlia/iot/plugin/swiftpass/requestor/SwiftpassDefaultApiHttpRequestor.java
+++ xapi/iot-swiftpass/src/main/java/com/minlia/iot/plugin/swiftpass/requestor/SwiftpassDefaultApiHttpRequestor.java
@@ -1,18 +1,18 @@
 package com.minlia.iot.plugin.swiftpass.requestor;
 
-import com.minlia.iot.plugin.swiftpass.body.SwiftpassApiHttpRequestBody;
 import com.minlia.iot.body.response.StatefulApiResponseBody;
+import com.minlia.iot.plugin.swiftpass.body.SwiftpassApiHttpRequestBody;
 import com.minlia.iot.plugin.swiftpass.body.SwiftpassApiHttpResponseBody;
 import com.minlia.iot.plugin.swiftpass.config.SwiftpassApiCredentialConfiguration;
 import com.minlia.iot.processor.ApiProcessor;
 import com.minlia.iot.requestor.DefaultApiHttpRequestor;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by will on 9/10/17.
  */
 public class SwiftpassDefaultApiHttpRequestor<REQUEST extends SwiftpassApiHttpRequestBody, RESPONSE extends SwiftpassApiHttpResponseBody> extends
     DefaultApiHttpRequestor<REQUEST, RESPONSE> {
-
 
   public SwiftpassDefaultApiHttpRequestor( ApiProcessor<REQUEST, RESPONSE> apiProcessor ) {
     super(apiProcessor);
@@ -24,13 +24,13 @@ public class SwiftpassDefaultApiHttpRequestor<REQUEST extends SwiftpassApiHttpRe
   @Override
   public StatefulApiResponseBody<RESPONSE> request(REQUEST body) {
 
-    //封装一些通用参数进来, 基本是每次请求都需要的参数
-    body.setMchId(((SwiftpassApiCredentialConfiguration) getApiProcessor().getContext()
-        .getPreferApiCredentialConfiguration()).getMchId());
-
+    if (StringUtils.isEmpty(body.getMchId())) {
+      //封装一些通用参数进来, 基本是每次请求都需要的参数
+      body.setMchId(((SwiftpassApiCredentialConfiguration) getApiProcessor().getContext()
+              .getPreferApiCredentialConfiguration()).getMchId());
+    }
     //最终调用处理器, 进行各种处理
     return super.request(body);
   }
-
 
 }
